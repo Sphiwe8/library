bdad9c32c6e8340792e87ec390a93494b8d22022
Sphiwe8 <109581436+Sphiwe8@users.noreply.github.com>
2022-09-08 11:49:05 +0200

    first commit

---
+++ collections.js
@@ -0,0 +1,33 @@
+function openPage(){
+
+var search=document.getElementById("search").value;
+
+if(search === "holy bible"){
+    window.open("/book1.html")
+}
+
+if(search === "communion"){
+  window.open("/book2.html")
+}
+
+
+if(search === "waiting for meeting"){
+  window.open("/book3.html")
+}
+
+
+if(search === "american dad"){
+  window.open("/book4.html")
+}
+
+
+if(search === "the sultan's daughter"){
+  window.open("/book5")
+}
+
+
+if(search === "rick and morty"){
+  window.open("/book6")
+}
+
+}
---
+++ favourites.js
@@ -0,0 +1,66 @@
+function favourites(){
+
+    var search=document.getElementById("search").value;
+    
+    if(search === "advebture time"){
+        window.open("/book1.html")
+    }
+    
+    if(search === "the amazing world of gumball"){
+      window.open("/book2.html")
+    }
+    
+    
+    if(search === "the simpsons"){
+      window.open("/book3.html")
+    }
+    
+    
+    if(search === "rick and morty"){
+      window.open("/book4.html")
+    }
+    
+    
+    if(search === "the amazing world of gumball"){
+      window.open("/book5.html")
+    }
+    
+    
+    if(search === "regular show"){
+      window.open("/book6.html")
+    }
+    
+    
+    if(search === "adventure time"){
+        window.open("/book7.html")
+      }
+
+      
+    if(search === "american dad"){
+        window.open("/book8.html")
+      }
+
+      
+    if(search === "the simpsons"){
+        window.open("/book9.html")
+      }
+
+      
+    if(search === "rick and morty"){
+        window.open("/book10.html")
+      }
+
+      
+    if(search === "family guy"){
+        window.open("/book11.html")
+      }
+
+      
+    if(search === "family guy"){
+        window.open("/book12.html")
+      }
+
+    
+    }
+
+    
